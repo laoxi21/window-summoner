0beaf2147beb06f58621619bf498c10cd5a60093
laoxi21 <87528129+laoxi21@users.noreply.github.com>
2026-05-08 13:37:52 +0800

    Change output filename in compile.bat

--- compile.bat
+++ compile.bat
@@ -6,6 +6,6 @@ rm release\%version% -rf
 mkdir release\%version%
 cp data release\%version%\data -rf
 rm release\%version%\data\config.json
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\������ȥ.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "app.ahk" /icon "data\icon.ico" /out "release\%version%\呼来唤去.exe" /bin "C:\Program Files\AutoHotkey\ahk_h v2.1-alpha.7\AutoHotkey64.exe"
 @REM bandizip compress to zip
 "C:\Program Files\Bandizip\Bandizip.exe" c -y -r "release\%version%.zip" "release\%version%"
